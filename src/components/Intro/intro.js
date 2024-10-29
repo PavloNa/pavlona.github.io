a23bbf241c7a9bb3a9b0030ae649b14d5b50f8a6
Pavlo <pavlo.escola@gmail.com>
2024-10-29 01:20:35 +0000

    Font update

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -6,10 +6,10 @@ import briefcase from '../../assets/briefcase.png'
 const Intro = () => {
   return (
     <section id='intro'>
-        <div className='IntroContent'>
+        <div className='introContent'>
             <span className='hello'>Hello,</span>
-            <span className='intoText'>I am <span className='intoName'>Pavlo</span><br/>Developer</span>
-            <p className='intoParagraph'>I am a passionate programmer looking to further show my skills in the developer world.<br/>I am eager to take on multiple challenges and contribute as much as I can for the benefit of everyone.</p>
+            <span className='intoText'>I am <span className='introName'>Pavlo</span><br/>Developer</span>
+            <p className='introParagraph'>I am a passionate programmer looking to further show my skills in the developer world.<br/>I am eager to take on multiple challenges and contribute as much as I can for the benefit of everyone.</p>
             <Link><button className='btn'><img src={briefcase} alt='' className='btnImg'></img>Hire me!</button></Link>
         </div>
         <img src='' alt='' className='backImg'></img>
