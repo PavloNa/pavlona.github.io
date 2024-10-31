b2d6e1aef706e3881cc47b0e9446dc59fe3b4dd5
Pavlo <pavlo.escola@gmail.com>
2024-10-31 12:33:43 +0000

    Color changes, added typing effect

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './intro.css'
 import {Link} from 'react-scroll'
 import briefcase from '../../assets/briefcase.png'
+import {ReactTyped} from 'react-typed'
 
 const Intro = () => {
   return (
     <section id='intro'>
         <div className='introContent'>
             <span className='hello'>Hello,</span>
-            <span className='intoText'>I am <span className='introName'>Pavlo</span><br/>Developer</span>
+            <span className='intoText'>I am <span className='introName'>Pavlo</span><br/>
+            <ReactTyped strings={["Developer", "Programmer", "Software Engineer"]} typeSpeed={80} backSpeed={80} loop/></span>
             <p className='introParagraph'>I am a passionate programmer looking to further show my skills in the developer world.<br/>I am eager to take on multiple challenges and contribute as much as I can for the benefit of everyone.</p>
             <Link><button className='btn'><img src={briefcase} alt='' className='btnImg'></img>Hire me!</button></Link>
         </div>
