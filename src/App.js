bcbaf6807d6884d554f93e56bb8b0b9652f985a6
Pavlo <pavlo.escola@gmail.com>
2024-10-31 13:25:29 +0000

    Added phone responsiveness, added Skills section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Intro from "./components/Intro/intro";
 import NavBar from "./components/NavBar/navbar";
+import Skills from "./components/Skills/skills";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Intro/>
+      <NavBar />
+      <Intro />
+      <Skills />
     </div>
   );
 }
--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,18 +3,19 @@ import './intro.css'
 import {Link} from 'react-scroll'
 import briefcase from '../../assets/briefcase.png'
 import {ReactTyped} from 'react-typed'
+import me from '../../assets/me.jpg'
 
 const Intro = () => {
   return (
     <section id='intro'>
         <div className='introContent'>
             <span className='hello'>Hello,</span>
-            <span className='intoText'>I am <span className='introName'>Pavlo</span><br/>
-            <ReactTyped strings={["Developer", "Programmer", "Software Engineer"]} typeSpeed={80} backSpeed={80} loop/></span>
+            <span className='introText'>I am <span className='introName'>Pavlo</span><br/>
+            <span className='changingText'>I'm a <ReactTyped strings={["Developer", "Programmer", "Software Engineer"]} typeSpeed={80} backSpeed={80} loop/></span></span>
             <p className='introParagraph'>I am a passionate programmer looking to further show my skills in the developer world.<br/>I am eager to take on multiple challenges and contribute as much as I can for the benefit of everyone.</p>
             <Link><button className='btn'><img src={briefcase} alt='' className='btnImg'></img>Hire me!</button></Link>
         </div>
-        <img src='' alt='' className='backImg'></img>
+        <img src={me} alt='' className='backImg'></img>
     </section>
   )
 }
